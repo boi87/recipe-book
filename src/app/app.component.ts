e9ddada2b4c32365ddb7fb68b9fe7283eef6bf51
Michele <boimik87@gmail.com>
2019-04-11 11:40:25 +0100

    recipe and shoppingList components visibility is toggled on click of header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Recipe Book';
+  title = "Recipe Book";
+  recipesVisibility;
+  shoppingListVisibility;
+  constructor() {}
+
+  ngOnInit() {}
+
+  onRecipesVisibilityFired(visible: boolean) {
+    console.log("recipes visibility in app,", visible);
+    this.recipesVisibility = visible;
+  }
+
+  onShoppingListVisibilityFired(visible: boolean) {
+    console.log("shopping visibility in app,", visible);
+    this.shoppingListVisibility = visible;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+// import { EventEmitter } from "events";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  @Output() recipesVisibilityFired = new EventEmitter<boolean>();
+  @Output() ShoppingListVisibilityFired = new EventEmitter<boolean>();
+  recipesVisible = false;
+  shoppingListVisible = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  toggleRecipesVisibility() {
+    this.recipesVisible = !this.recipesVisible;
+    this.recipesVisibilityFired.emit(this.recipesVisible);
+    console.log(this.recipesVisible);
   }
 
+  toggleShoppingListVisibility() {
+    this.shoppingListVisible = !this.shoppingListVisible;
+    this.ShoppingListVisibilityFired.emit(this.shoppingListVisible);
+    console.log(this.shoppingListVisible);
+  }
 }
