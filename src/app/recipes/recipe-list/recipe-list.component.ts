11c49a743e80c7d0f15aa31d7ca80bf6ec618a5e
Michele <boimik87@gmail.com>
2019-04-15 17:11:47 +0100

    adds recipe.service.ts for the recipe component and cleans up events in code

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipes.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,25 +8,12 @@ import { Recipe } from '../recipes.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
+  constructor(private recipeService: RecipeService) {}
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Vino',
-      'Rosso',
-      'https://s.tannico.it/media/catalog/product/cache/2/thumbnail/0dc2d03fe217f8c83829496872af24a0/8/0/8055681240063_01.jpg'
-    ),
-    new Recipe(
-      'Vino',
-      'Bianco',
-      'https://s.tannico.it/media/catalog/product/cache/43/thumbnail/0dc2d03fe217f8c83829496872af24a0/b/i/bia_3.jpg'
-    )
-  ];
-  constructor() {}
-
-  ngOnInit() {}
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
+
+
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,25 @@
+import { Recipe } from './recipes.model';
+import {EventEmitter} from '@angular/core';
+// import {Recipe} from './recipes.model';
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Vino',
+      'Rosso',
+      'https://s.tannico.it/media/catalog/product/cache/2/thumbnail/0dc2d03fe217f8c83829496872af24a0/8/0/8055681240063_01.jpg'
+    ),
+    new Recipe(
+      'Vino',
+      'Bianco',
+      'https://s.tannico.it/media/catalog/product/cache/43/thumbnail/0dc2d03fe217f8c83829496872af24a0/b/i/bia_3.jpg'
+    )
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
