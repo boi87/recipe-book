1c7bc6766c9ed4c4db2c0e7ccb95b7a1931bfcc6
Michele <boimik87@gmail.com>
2019-04-16 13:16:56 +0100

    ingredients adds to recipe and renders

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,6 @@
 import { Recipe } from './recipes.model';
 import { EventEmitter } from '@angular/core';
-// import {Recipe} from './recipes.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -9,12 +9,14 @@ export class RecipeService {
     new Recipe(
       'Vino',
       'Rosso',
-      'https://s.tannico.it/media/catalog/product/cache/2/thumbnail/0dc2d03fe217f8c83829496872af24a0/8/0/8055681240063_01.jpg'
+      'https://s.tannico.it/media/catalog/product/cache/2/thumbnail/0dc2d03fe217f8c83829496872af24a0/8/0/8055681240063_01.jpg',
+      [new Ingredient('White Grapes', 100), new Ingredient('Sulphites', 20)]
     ),
     new Recipe(
       'Vino',
       'Bianco',
-      'https://s.tannico.it/media/catalog/product/cache/43/thumbnail/0dc2d03fe217f8c83829496872af24a0/b/i/bia_3.jpg'
+      'https://s.tannico.it/media/catalog/product/cache/43/thumbnail/0dc2d03fe217f8c83829496872af24a0/b/i/bia_3.jpg',
+      [new Ingredient('Red Grapes', 100), new Ingredient('Sulphites', 1)] // thanks to constructor
     )
   ];
 
