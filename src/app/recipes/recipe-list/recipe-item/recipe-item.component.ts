60fdb50d6d09c0254bdbc8bdd957d2e3733cb6be
Michele <boimik87@gmail.com>
2019-04-18 16:48:09 +0100

    all broken. implementing routes

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Recipe } from "../../recipes.model";
+import {Component, OnInit, Input} from '@angular/core';
+import {Recipe} from '../../recipes.model';
 import {RecipeService} from '../../recipe.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
-  selector: "app-recipe-item",
-  templateUrl: "./recipe-item.component.html",
-  styleUrls: ["./recipe-item.component.css"]
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   onSelected() {
-this.recipeService.recipeSelected.emit(this.recipe)
+    this.recipeService.recipeSelected.emit(this.recipe);
+    this.router.navigate(['/recipes', this.recipe.id], {relativeTo: this.route});
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,20 @@
-import { Recipe } from './recipes.model';
-import { EventEmitter } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
+import {Recipe} from './recipes.model';
+import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
+      1,
       'Wine',
       'Red, full bodied.',
       'https://s.tannico.it/media/catalog/product/cache/2/thumbnail/0dc2d03fe217f8c83829496872af24a0/8/0/8055681240063_01.jpg',
       [new Ingredient('Red Grapes', 100), new Ingredient('Sulphites', 20)]
     ),
     new Recipe(
+      2,
       'Wine',
       'White, crisp and refreshing',
       'https://s.tannico.it/media/catalog/product/cache/43/thumbnail/0dc2d03fe217f8c83829496872af24a0/b/i/bia_3.jpg',
